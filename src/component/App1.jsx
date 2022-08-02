e9806e4ed880860bd66b7f1bb4b78067efd2f10a
mehranbahari <mehranbahari20@gmail.com>
2022-08-02 16:37:40 +0430

    first commit

---
+++ src/component/App1.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+const App = () => {
+  const [show, setShow] = useState({});
+  const [active, setActive] = useState("");
+
+  //   const m = 1112cf0f6e8f4e7daf573559212512
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(
+        `http://api.weatherapi.com/v1/current.json?key= 1112cf0f6e8f4e7daf573559212512&q=${active}&aqi=no`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setShow(data);
+          setActive("");
+          console.log(data);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div className={(typeof show.location != "undefined") ? ((show.current.temp_c > 16) ? "app warm" : "app") : "app"}>
+      <main>
+        <div className="search-box">
+          <input
+            className="search-bar"
+            value={active}
+            onChange={(e) => setActive(e.target.value)}
+            placeholder="search"
+            onKeyPress={search}
+          />
+        </div>
+        {typeof show.location != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {show.location.country},{show.location.name}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{show.current.temp_c}C</div>
+              <div className="weather">
+                <img src={show.current.condition.icon} alt="" />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default App;
